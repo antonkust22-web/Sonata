718d8be9cd54ac13d9c30f7d9561c5f20b267274
antonkust22-web <antonkust22@icloud.com>
2026-03-29 15:22:58 +0300

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -4,11 +4,11 @@ from aiogram.filters import Command
 import asyncio
 
 text1 = (
-"Обходите белый список легко!"
+"Обходите белый список легко!",
  
-"✅Невидим для DPI (глубокий анализ трафик)                "
-"✅Работает в строгих сетях (корпоративных, учебных)                   "
-"✅Простое подключение в один клик              "
+"✅Невидим для DPI (глубокий анализ трафик)                ",
+"✅Работает в строгих сетях (корпоративных, учебных)                   ",
+"✅Простое подключение в один клик              ",
 " "
 "дальше здесь будет информция о подписке"
 )
@@ -33,7 +33,7 @@ def get_inline_keyboard():
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(text1, parse_mode="HTML", reply_markup=get_inline_keyboard())
-
+    video_url = "https://clck.ru/3SpDww"
 # 3. Обрабатываем нажатия
 @dp.callback_query(F.data == "like")
 async def send_random_value(callback: types.CallbackQuery):
@@ -49,6 +49,7 @@ async def send_random_value(callback: types.CallbackQuery):
 async def send_random_value(callback: types.CallbackQuery):
     await callback.answer("Вам не понравилось!")
     await callback.message.edit_text("Здесь будут условия, цены и так далее")
+def get_inline_keyboard():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Цена и на сколько дней", callback_data="salе1")] 
     ])
