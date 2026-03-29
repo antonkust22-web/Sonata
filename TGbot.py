8f61516b80505ea2016f675f730e2e19a605595b
antonkust22-web <antonkust22@icloud.com>
2026-03-29 18:58:34 +0300

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -6,9 +6,9 @@ import asyncio
 text1 = (
 "Обходите белый список легко!"
  
-"✅Невидим для DPI (глубокий анализ трафик)                "
-"✅Работает в строгих сетях (корпоративных, учебных)                   "
-"✅Простое подключение в один клик              "
+"✅Невидим для DPI"
+"✅Работает в строгих сетях"
+"✅Простое подключение в один клик"
 " "
 "дальше здесь будет информция о подписке"
 )
@@ -33,6 +33,7 @@ def get_inline_keyboard():
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(text1, parse_mode="HTML", reply_markup=get_inline_keyboard())
+    video_url = "https://clck.ru/3SpDww"
 
 # 3. Обрабатываем нажатия
 @dp.callback_query(F.data == "like")
