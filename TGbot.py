09399ea362d94628aa91084ffed2bd1c2993c002
antonkust22-web <antonkust22@icloud.com>
2026-06-17 09:17:03 +0000

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -35,7 +35,7 @@ logging.basicConfig(
 )
 
 # --- Настройки (ОБЯЗАТЕЛЬНО ОБНОВИТЕ ТОКЕН И ПАРОЛЬ) ---
-API_TOKEN = '8728088789:AAGfyqAhbg2Ola2BE3n5duGV_LKPgPcT6AI'
+API_TOKEN = '8728088789:AAFZSnTY46Z2v2-5hk3Henv5JBSkHXi5avQ'
 
 
 
