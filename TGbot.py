aeb0531cb846c2d121097c2bff84ac3030ed66ba
antonkust22-web <antonkust22@icloud.com>
2026-07-14 19:02:49 +0300

    обнова видео

--- TGbot.py
+++ TGbot.py
@@ -51,7 +51,7 @@ API_TOKEN = '8728088789:AAFZSnTY46Z2v2-5hk3Henv5JBSkHXi5avQ'
 PROVIDER_TOKEN = "390540012:LIVE:96775"
 
 # File ID вашего видео
-VIDEO_MAIN = "BAACAgIAAxkBAAEsJwVqVAWUDSpNCVuTFibc57pk9bFqIAACvqQAAhBIoUqct99GgxsPVTwE"
+VIDEO_MAIN = "BAACAgIAAxkBAAPmalZdX_GloRR8mPKqiXL6IEoFFpQAAs-iAALRx7BKu4dtBUXOXuk9BA"
 
 text1 = (
     "<b>👋 Привет, добро пожаловать в наш VPN сервис</b>\n\n"
