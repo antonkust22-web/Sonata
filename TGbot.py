27a157365d9dee2c53ec2dc9ad9e99d5a87436c8
antonkust22-web <antonkust22@icloud.com>
2026-09-20 16:18:17 +0300

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -2696,15 +2696,16 @@ async def cabinet(callback: types.CallbackQuery):
 
         # Сборка итогового сообщения
         text = (
-            f"<b>👤 Личный кабинет</b>\n\n" # Синяя кликабельная команда
-            f"<pre>"
+            f"<b>👤 Личный кабинет</b>\n\n"
+            f'<pre language="docker">'
             f"{role_badge}\n"
-            f"<b>ID пользователя:</b> <code>{user_id}</code>\n"
-            f"<b>Статус подписки:</b> {status_text}\n"
-            f"{devices_text_block.strip()}" # Фикс лишней строки
-            f"</pre>"
-            f"{ref_text_block}"
+            f"ID пользователя: {user_id}\n"
+            f"Статус подписки: {status_text}\n"
+            f"{devices_text_block}"
+            f"</pre>"  # Тег прижат вплотную к переменной без \n
+            f"\n\n{ref_text_block}"
         )
+
 
 
 
