c32f541beb16a6c2574c6bc83fd17cd30fa41c36
antonkust22-web <antonkust22@icloud.com>
2026-09-22 14:06:02 +0300

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -917,7 +917,7 @@ SERVERS = [
     },
     {
         "id": "de_1",
-        "panel_url": "https://sonatavpn.ru", 
+        "panel_url": "http://78.17.152.36:2053", 
         "base_path": "/dsjwEGmmrbon",
         "panel_user": "Soul",
         "panel_password": "Lodka1321",
